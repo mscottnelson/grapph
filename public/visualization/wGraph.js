a60d5742b205ef7e3763f233383b10ff17b9302b
Michael Scott-Nelson <mscottnelson@gmail.com>
2016-11-28 20:37:10 -0500

    fix visualization setup, begin weight creation fixes

---
+++ public/visualization/wGraph.js
@@ -0,0 +1,31 @@
+var d3 = require('d3');
+var nv = require('nvd3');
+
+d3.json('../staticData/cumulativeLineData.json', function(data) {
+  nv.addGraph(function() {
+    var chart = nv.models.cumulativeLineChart()
+                  .x(function(d) { return d[0] })
+                  .y(function(d) { return d[1]/100 }) //adjusting, 100% is 1.00, not 100 as it is in the data
+                  .color(d3.scale.category10().range())
+                  .useInteractiveGuideline(true)
+                  ;
+
+     chart.xAxis
+        .tickValues([1078030800000,1122782400000,1167541200000,1251691200000])
+        .tickFormat(function(d) {
+            return d3.time.format('%x')(new Date(d))
+          });
+
+    chart.yAxis
+        .tickFormat(d3.format(',.1%'));
+
+    d3.select('#chart svg')
+        .datum(data)
+        .call(chart);
+
+    //TODO: Figure out a good way to do this automatically
+    nv.utils.windowResize(chart.update);
+
+    return chart;
+  });
+});
