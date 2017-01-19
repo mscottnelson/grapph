310a55a750ed06eb9930239a98666e85d4cf4552
Michael Scott-Nelson <mscottnelson@gmail.com>
2017-01-18 17:14:58 -0800

    add jspm packages for heroku

---
+++ public/jspm_packages/npm/jsdom@1.0.0/lib/jsdom/level2/style.js
@@ -0,0 +1,151 @@
+/* */ 
+var core = require('./core').dom.level2.core,
+    html = require('./html').dom.level2.html,
+    utils = require('../utils'),
+    defineGetter = utils.defineGetter,
+    defineSetter = utils.defineSetter,
+    inheritFrom = utils.inheritFrom,
+    cssom = require('cssom'),
+    cssstyle = require('../../../node_modules/cssstyle-browserify/lib/CSSStyleDeclaration'),
+    assert = require('assert');
+core.StyleSheet = cssom.StyleSheet;
+core.MediaList = cssom.MediaList;
+core.CSSStyleSheet = cssom.CSSStyleSheet;
+core.CSSRule = cssom.CSSRule;
+core.CSSStyleRule = cssom.CSSStyleRule;
+core.CSSMediaRule = cssom.CSSMediaRule;
+core.CSSImportRule = cssom.CSSImportRule;
+core.CSSStyleDeclaration = cssstyle.CSSStyleDeclaration;
+core.StyleSheetList = function() {
+  this._length = 0;
+};
+core.StyleSheetList.prototype = {
+  item: function(i) {
+    return this[i];
+  },
+  push: function(sheet) {
+    this[this._length] = sheet;
+    this._length++;
+  },
+  get length() {
+    return this._length;
+  }
+};
+defineGetter(core.Document.prototype, 'styleSheets', function() {
+  if (!this._styleSheets) {
+    this._styleSheets = new core.StyleSheetList();
+  }
+  return this._styleSheets;
+});
+function fetchStylesheet(url, sheet) {
+  html.resourceLoader.load(this, url, function(data, filename) {
+    sheet.href = html.resourceLoader.resolve(this.ownerDocument, url);
+    evaluateStylesheet.call(this, data, sheet, url);
+  });
+}
+function evaluateStylesheet(data, sheet, baseUrl) {
+  var newStyleSheet = cssom.parse(data);
+  var spliceArgs = newStyleSheet.cssRules;
+  spliceArgs.unshift(0, sheet.cssRules.length);
+  Array.prototype.splice.apply(sheet.cssRules, spliceArgs);
+  scanForImportRules.call(this, sheet.cssRules, baseUrl);
+  this.ownerDocument.styleSheets.push(sheet);
+}
+function scanForImportRules(cssRules, baseUrl) {
+  if (!cssRules)
+    return;
+  for (var i = 0; i < cssRules.length; ++i) {
+    if (cssRules[i].cssRules) {
+      scanForImportRules.call(this, cssRules[i].cssRules, baseUrl);
+    } else if (cssRules[i].href) {
+      fetchStylesheet.call(this, cssRules[i].href, this.sheet);
+    }
+  }
+}
+function evaluateStyleAttribute(data) {}
+function StyleAttr(node, value) {
+  this._node = node;
+  core.Attr.call(this, node.ownerDocument, 'style');
+  if (!this._node._ignoreValueOfStyleAttr) {
+    this.nodeValue = value;
+  }
+}
+inheritFrom(core.Attr, StyleAttr, {
+  get nodeValue() {
+    if (typeof this._node._style === 'string') {
+      return this._node._style;
+    } else {
+      return this._node.style.cssText;
+    }
+  },
+  set nodeValue(value) {
+    this._node._style = value;
+  }
+});
+utils.intercept(core.AttributeList, '$setNode', function(_super, args, attr) {
+  if (attr.name == 'style') {
+    attr = new StyleAttr(this._parentNode, attr.nodeValue);
+  }
+  return _super.call(this, attr);
+});
+defineGetter(html.HTMLElement.prototype, 'style', function() {
+  if (typeof this._style === 'string') {
+    var styleSheet = cssom.parse('dummy{' + this._style + '}');
+    this._style = new cssstyle.CSSStyleDeclaration();
+    if (styleSheet.cssRules.length > 0 && styleSheet.cssRules[0].style) {
+      var newStyle = styleSheet.cssRules[0].style;
+      for (var i = 0; i < newStyle.length; ++i) {
+        var prop = newStyle[i];
+        this._style.setProperty(prop, newStyle.getPropertyValue(prop), newStyle.getPropertyPriority(prop));
+      }
+    }
+  }
+  if (!this._style) {
+    this._style = new cssstyle.CSSStyleDeclaration();
+  }
+  if (!this.getAttributeNode('style')) {
+    this._ignoreValueOfStyleAttr = true;
+    this.setAttribute('style');
+    this._ignoreValueOfStyleAttr = false;
+  }
+  return this._style;
+});
+assert.equal(undefined, html.HTMLLinkElement._init);
+html.HTMLLinkElement._init = function() {
+  this.addEventListener('DOMNodeInsertedIntoDocument', function() {
+    if (!/(?:[ \t\n\r\f]|^)stylesheet(?:[ \t\n\r\f]|$)/i.test(this.rel)) {
+      return;
+    }
+    if (this.href) {
+      fetchStylesheet.call(this, this.href, this.sheet);
+    }
+  });
+  this.addEventListener('DOMNodeRemovedFromDocument', function() {});
+};
+var getOrCreateSheet = function() {
+  if (!this._cssStyleSheet) {
+    this._cssStyleSheet = new cssom.CSSStyleSheet();
+  }
+  return this._cssStyleSheet;
+};
+defineGetter(html.HTMLLinkElement.prototype, 'sheet', getOrCreateSheet);
+assert.equal(undefined, html.HTMLStyleElement._init);
+html.HTMLStyleElement._init = function() {
+  this.addEventListener('DOMNodeInsertedIntoDocument', function() {
+    if (this.type && this.type !== 'text/css') {
+      return;
+    }
+    var content = '';
+    Array.prototype.forEach.call(this.childNodes, function(child) {
+      if (child.nodeType === child.TEXT_NODE) {
+        content += child.nodeValue;
+      }
+    });
+    evaluateStylesheet.call(this, content, this.sheet, this._ownerDocument.URL);
+  });
+};
+defineGetter(html.HTMLStyleElement.prototype, 'sheet', getOrCreateSheet);
+exports.dom = {level2: {
+    html: html,
+    core: core
+  }};
---
+++ public/jspm_packages/npm/nvd3@1.8.4.js
@@ -0,0 +1,1 @@
+module.exports = require("npm:nvd3@1.8.4/build/nv.d3.js");
